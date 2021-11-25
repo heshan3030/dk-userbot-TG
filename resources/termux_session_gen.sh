8ac9d84e31e91ff094a5912548b3f4bd6f0925d4
@Dk_king_offcial <92113310+heshan3030@users.noreply.github.com>
2021-11-25 11:28:16 -0800

    Update termux_session_gen.sh

--- resources/termux_session_gen.sh
+++ resources/termux_session_gen.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 clear
 echo "
-██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗  ██╗░░░░░██╗░██████╗░██╗░░██╗████████╗███╗░░██╗██╗███╗░░██╗░██████╗░
-██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝  ██║░░░░░██║██╔════╝░██║░░██║╚══██╔══╝████╗░██║██║████╗░██║██╔════╝░
-██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░  ██║░░░░░██║██║░░██╗░███████║░░░██║░░░██╔██╗██║██║██╔██╗██║██║░░██╗░
-██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░  ██║░░░░░██║██║░░╚██╗██╔══██║░░░██║░░░██║╚████║██║██║╚████║██║░░╚██╗
-██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗  ███████╗██║╚██████╔╝██║░░██║░░░██║░░░██║░╚███║██║██║░╚███║╚██████╔╝
-╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
-
-"
+▄︻デɖӄ-ӄɨռɢ-ʊֆɛʀ-ɮօȶ══━一"
 # Termux session string generator for TeleBot
 echo Starting dependency installation in 5 seconds...
 sleep 5
@@ -16,6 +9,6 @@ apt-get update
 apt-get upgrade -y
 pkg upgrade -y
 pkg install python wget -y
-wget https://raw.githubusercontent.com/KeinShin/Black-Lightning/master/telesetup.py
+wget https://raw.githubusercontent.com/heshan3030/dk-userbot-TG/master/telesetup.py
 pip3 install telethon
 python3 telesetup.py
