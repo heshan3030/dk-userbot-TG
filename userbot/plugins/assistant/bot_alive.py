9abb67d7cb212d263eb5accf9b95ba96a3f5521c
@Dk_king_offcial <92113310+heshan3030@users.noreply.github.com>
2021-11-25 14:07:11 -0800

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -9,7 +9,7 @@ from userbot import ALIVE_NAME, bot
 currentversion = "2.1"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DK-KING-USER-BOT"
 ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
 if ASSIS_PIC is None:
      PM_IMG = "https://telegra.ph/file/beb843ce962a738c52cad.jpg"
@@ -22,9 +22,9 @@ pm_caption += "► **SYSTEMS STATS**\n"
 pm_caption += "► **Telethon Version:** `1.15.0` \n"
 pm_caption += f"► **Assistant Version** : `{currentversion}`\n"
 pm_caption += f"► **My Master** : {DEFAULTUSER} \n"
-pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "► **Copyright** : [Black Lighting](GitHub.com/keinshin/Black-Lightning)\n"
-light = [[Button.url("✧Repos✧", "https://github.com/KeinShin/Black-Lightning"), Button.url("✧Support✧", "https://t.me/lightning_support_group")]]
+pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/heshan3030/dk-userbot-TG/blob/master/LICENSE)\n"
+pm_caption += "► **Copyright** : [DK-KING-USER-BOT](GitHub.com/heshan3030/dk-userbot-TG)\n"
+light = [[Button.url("✧Repos✧", "https://github.com/heshan3030/dk-userbot-TG"), Button.url("✧Support✧", "https://t.me/Dk_king_offcial")]]
 light +=[[custom.Button.inline("✧Help✧", data="gibcmd")]]
 @tgbot.on(events.NewMessage(pattern="^/alive" , func=lambda e: e.sender_id == bot.uid))
 async def _(event):
