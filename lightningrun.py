0b68ecc9aa3065103e6cf6d6de82e5e4288df481
@Dk_king_offcial <92113310+heshan3030@users.noreply.github.com>
2021-11-25 10:23:33 -0800

    dk-userbot-TG.py
    dk-userbot-TG.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/heshan3030/dk-userbot-TG /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/heshan3030/dk-userbot-TG /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot") 
